cf9934b9d40463f3969305361d71f7744e6c339a
snt-nbzw <sntnbzw@gmail.com>
2020-03-21 17:09:41 +0900

    Search Bar Controller and Search Throtting #9

--- AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -9,22 +9,72 @@
 import UIKit
 import SDWebImage
 
-class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     fileprivate let cellId = "id1234"
+
+    fileprivate let searchController = UISearchController(searchResultsController: nil)
+
+    fileprivate let enterSearchTermLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Please enter search term above..."
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .white
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
 
-        fetchItunesApps()
+        collectionView.addSubview(enterSearchTermLabel)
+        enterSearchTermLabel.fillSuperview(padding: .init(top: 100, left: 50, bottom: 0, right: 50))
+
+        setupSearchBar()
+
+//        fetchItunesApps()
+    }
+
+    fileprivate func setupSearchBar() {
+        definesPresentationContext = true
+        navigationItem.searchController = self.searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+
+        if #available(iOS 12.0, *) {
+            searchController.obscuresBackgroundDuringPresentation = false
+        } else {
+            searchController.dimsBackgroundDuringPresentation = false
+        }
+        searchController.searchBar.delegate = self
+    }
+
+    var timer: Timer?
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+
+        // introduce some delay before performing the search
+        // throtting the search
+
+        timer?.invalidate()
+
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+
+            // this will actually fire my search
+            Service.shared.fetchApps(searchTerm: searchText) { (res, err) in
+                self.appResults = res
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }
+        })
     }
 
     fileprivate var appResults = [Result]()
 
     fileprivate func fetchItunesApps() {
-        Service.shared.fetchApps { (results, err) in
+        Service.shared.fetchApps(searchTerm: "Twitter") { (results, err) in
 
             if let err = err {
                 print("Failed to fetch apps:, \(err)")
@@ -43,6 +93,7 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        enterSearchTermLabel.isHidden = appResults.count != 0
         return appResults.count
     }
 
