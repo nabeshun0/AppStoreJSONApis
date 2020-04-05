f331aba900b5288fa52f8ee0f6be1a02edd81c6b
snt-nbzw <sntnbzw@gmail.com>
2020-04-05 23:00:58 +0900

    app row cells #11

---
+++ AppStoreJSONApis/Views/Apps/AppRowCell.swift
@@ -0,0 +1,44 @@
+//
+//  AppRowCell.swift
+//  AppStoreJSONApis
+//
+//  Created by member on 2020/04/05.
+//  Copyright © 2020 Shunta Nabezawa. All rights reserved.
+//
+
+import UIKit
+
+class AppRowCell: UICollectionViewCell {
+
+    let imageView = UIImageView(cornerRadius: 8)
+
+    let nameLabel = UILabel(text: "App Name", font: .systemFont(ofSize: 20))
+    let companyLabel = UILabel(text: "Company Name", font: .systemFont(ofSize: 13))
+
+    let getButton = UIButton(title: "GET")
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        imageView.backgroundColor = .purple
+        imageView.constrainWidth(constant: 64)
+        imageView.constrainHeight(constant: 64)
+
+        getButton.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        getButton.constrainWidth(constant: 80)
+        getButton.constrainHeight(constant: 32)
+        getButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        getButton.layer.cornerRadius = 32 / 2
+
+        let stackView = UIStackView(arrangedSubviews: [imageView, VerticalStackView(arrangedSubview: [nameLabel, companyLabel], spacing: 4), getButton])
+        stackView.spacing = 16
+        stackView.alignment = .center
+        addSubview(stackView)
+        stackView.fillSuperview()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
