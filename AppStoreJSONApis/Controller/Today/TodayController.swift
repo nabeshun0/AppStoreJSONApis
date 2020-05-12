1b19457bdae72f954189c78cadb9ae54c5b4afea
snt-nbzw <sntnbzw@gmail.com>
2020-05-12 10:09:43 +0900

    Dynamic Today Cells #32

--- AppStoreJSONApis/Controller/Today/TodayController.swift
+++ AppStoreJSONApis/Controller/Today/TodayController.swift
@@ -12,14 +12,19 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
 
     fileprivate let cellId = "cellId"
 
+    let items = [
+        TodayItem.init(category: "LIFE HACK", title: "Utilizing your Time", image: #imageLiteral(resourceName: "garden"), description: "All the tools and apps you nee to intellingently organize your life the right way.", backgroundColor: .white),
+        TodayItem.init(category: "HOLIDAYS", title: "Travel on Budget", image: #imageLiteral(resourceName: "holiday"), description: "Find out all you need to know on how to travel without packing everything!", backgroundColor: #colorLiteral(red: 0.9842324853, green: 0.9647551179, blue: 0.7214853168, alpha: 1))
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.isNavigationBarHidden = true
         
         collectionView.backgroundColor = #colorLiteral(red: 0.9489366412, green: 0.9490727782, blue: 0.9489068389, alpha: 1)
+        
         collectionView.register(TodayCell.self, forCellWithReuseIdentifier: cellId)
-
     }
 
     var appFullscreenController: AppFullscreenController!
@@ -29,14 +34,15 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     var widthConstraint: NSLayoutConstraint?
     var heightConstraint: NSLayoutConstraint?
 
-
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 
         let appFullscreenController = AppFullscreenController()
-        let redView = appFullscreenController.view!
-        view.addSubview(redView)
-        redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView)))
-
+        appFullscreenController.todayItem = items[indexPath.row]
+        appFullscreenController.dismissHandler = {
+            self.handleRemoveRedView()
+        }
+        let fullScreenView = appFullscreenController.view!
+        view.addSubview(fullScreenView)
         addChild(appFullscreenController)
 
         self.appFullscreenController = appFullscreenController
@@ -50,16 +56,16 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
 
         // auto layout constraint animations
         // 4 anchors
-        redView.translatesAutoresizingMaskIntoConstraints = false
-        topConstraint = redView.topAnchor.constraint(equalTo: view.topAnchor, constant: startingFrame.origin.y)
-        leadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
-        widthConstraint = redView.widthAnchor.constraint(equalToConstant: startingFrame.width)
-        heightConstraint = redView.heightAnchor.constraint(equalToConstant: startingFrame.height)
+        fullScreenView.translatesAutoresizingMaskIntoConstraints = false
+        topConstraint = fullScreenView.topAnchor.constraint(equalTo: view.topAnchor, constant: startingFrame.origin.y)
+        leadingConstraint = fullScreenView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
+        widthConstraint = fullScreenView.widthAnchor.constraint(equalToConstant: startingFrame.width)
+        heightConstraint = fullScreenView.heightAnchor.constraint(equalToConstant: startingFrame.height)
 
         [topConstraint, leadingConstraint, widthConstraint, heightConstraint].forEach({$0?.isActive = true})
         self.view.layoutIfNeeded()
 
-        redView.layer.cornerRadius = 16
+        fullScreenView.layer.cornerRadius = 16
 
 
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
@@ -79,39 +85,36 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
 
     var startingFrame: CGRect?
 
-    @objc func handleRemoveRedView(gesture: UITapGestureRecognizer) {
+    @objc func handleRemoveRedView() {
 
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
 
             self.appFullscreenController.tableView.contentOffset = .zero
 
-            // this frame code is bad
-//            gesture.view?.frame = self.startingFrame ?? .zero
-
             guard let startingFrame = self.startingFrame else { return }
-
             self.topConstraint?.constant = startingFrame.origin.y
-                  self.leadingConstraint?.constant = startingFrame.origin.x
-                  self.widthConstraint?.constant = startingFrame.width
-                  self.heightConstraint?.constant = startingFrame.height
+            self.leadingConstraint?.constant = startingFrame.origin.x
+            self.widthConstraint?.constant = startingFrame.width
+            self.heightConstraint?.constant = startingFrame.height
 
-                  self.view.layoutIfNeeded() // starts animation
+            self.view.layoutIfNeeded() // starts animation
 
             self.tabBarController?.tabBar.transform = .identity
 
         }, completion: { _ in
-            gesture.view?.removeFromSuperview()
+            self.appFullscreenController.view.removeFromSuperview()
             self.appFullscreenController.removeFromParent()
         })
 
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-         return 4
+        return items.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! TodayCell
+        cell.todayItem = items[indexPath.item]
         return cell
     }
 
---
+++ AppStoreJSONApis/Model/TodayItem.swift
@@ -0,0 +1,19 @@
+//
+//  TodayItem.swift
+//  AppStoreJSONApis
+//
+//  Created by member on 2020/05/11.
+//  Copyright © 2020 Shunta Nabezawa. All rights reserved.
+//
+
+import UIKit
+
+struct TodayItem {
+
+    let category: String
+    let title: String
+    let image: UIImage
+    let description: String
+    let backgroundColor: UIColor
+
+}
